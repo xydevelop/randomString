cb9ed755ec36903d93237fce020a7422a91c692d
xydevelop <44612019+xydevelop@users.noreply.github.com>
2018-10-30 21:27:00 +0300

    Add Plugin Description

--- random-string-xy___.php
+++ random-string-xy___.php
@@ -1,1 +1,11 @@
-<?php echo 'hello World!!!!';
+<?php 
+
+/*
+Plugin Name: Random String-xy___
+Plugin URI: //xystudio.ru
+Description: Get Random String.
+Version: 1.0
+Author: Denis R.
+Author URI: //de.z.org
+*/
+
